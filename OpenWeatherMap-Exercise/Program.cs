489bbe012f9222c728f7be2942e4b9d4a0aa9229
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-03-16 14:54:51 -0400

    Finished version of Open Weather API Exercise

--- OpenWeatherMap-Exercise/Program.cs
+++ OpenWeatherMap-Exercise/Program.cs
@@ -8,19 +8,30 @@ namespace OpenWeatherMap_Exercise
     {
         static void Main(string[] args)
         {
-            // TODO: Create an instance of the HttpClient Class called client
+            // API Key: 6e818e76332207b1216990927bad79d2
+            // Create an instance of the HttpClient Class called client
+            var client = new HttpClient();
 
-            // TODO: Ask for the users API Key and store that in a variable called "api_Key"
+            // Ask for the users API Key and store that in a variable called "api_Key"
+            Console.Write("Welcome to the Open Weather API!\nPlease input your API Key: ");
+            var api_key = Console.ReadLine();
 
-            // TODO: Ask the user for their city name and store that in a variable called "city_name"
+            // Ask the user for their city name and store that in a variable called "city_name"
+            Console.Write("\nThank you.\n\nEnter the city name you wish to view the current weather forcast of: ");
+            var city_name = Console.ReadLine().ToLower();
 
-            // TODO: Create a variable to store the URL (use String Interpolation for the {city_name} and {api_Key}  HINT: Make sure to use the "imperial" measurement endpoint
+            // Create a variable to store the URL (use String Interpolation for the {city_name} and {api_Key}  HINT: Make sure to use the "imperial" measurement endpoint
+            var userURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial";
 
-            // TODO: Create a variable to store the response from your GET request to that URL from above  HINT: Don't forget to call .Result 
+            // Create a variable to store the response from your GET request to that URL from above  HINT: Don't forget to call .Result 
+            var weatherResponse = client.GetStringAsync(userURL).Result;
 
-            // TODO: Create a variable to store the formattedResponse after you JObject.Parse() the response from above
+            // Create a variable to store the formattedResponse after you JObject.Parse() the response from above
+            var formattedResponseMain = JObject.Parse(weatherResponse).GetValue("main").ToString();
+            var formattedResponseTemp = JObject.Parse(formattedResponseMain).GetValue("temp").ToString();
 
-            // TODO: Write out the current temperature in degrees Fahrenheit
+            // Write out the current temperature in degrees Fahrenheit
+            Console.WriteLine($"\n{formattedResponseTemp} degrees Fahrenheit.");
         }
     }
 }
